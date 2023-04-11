45842add81a820c0355102718fa9cb7de474baaa
Fernando Diaz <70718713+Metaldev-06@users.noreply.github.com>
2023-04-10 21:17:56 -0300

    version 0.1

--- handlers/getAllUser.ts
+++ handlers/getAllUser.ts
@@ -1,7 +1,7 @@
 import { Context } from "https://deno.land/x/oak@v12.1.0/mod.ts";
 
 export async function getAllUser(ctx: Context) {
-  const url = "https://jsonplaceholder.typicode.com/users";
+  const url = "https://jsonplaceholder.typicode.com/photos";
   try {
     const response = await fetch(`${url}`);
     const data = await response.json();
